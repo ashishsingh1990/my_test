8b699bffe40ee852943312a7a2b8a046aa646a47
rajesh1285 <rajeshnuthalapati499@gmail.com>
2018-04-22 03:36:08 +0530

    chagnes done

--- Gemfile
+++ Gemfile
@@ -10,7 +10,7 @@ end
 gem 'rails', '~> 5.1.5'
 # Use mysql as the database for Active Record
 # gem 'mysql2', '>= 0.3.18', '< 0.5'
-gem 'pg'
+# gem 'pg'
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
 # Use SCSS for stylesheets
@@ -36,7 +36,9 @@ gem "haml-rails", "~> 1.0"
 gem 'jquery-rails'
 gem 'devise'
 gem "select2-rails"
-gem 'rails_12factor'
+gem 'rails_12factor', group: :production
+gem 'pg', group: :production
+
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,9 +1,14 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.cache_classes = true
+  config.serve_static_assets = true
+  config.assets.compile = true
+  config.assets.digest = true
   # Code is not reloaded between requests.
   config.cache_classes = true
-
+  config.assets.precompile += %w( public.css public.js )   
+  config.assets.precompile += %w( assets/stylesheets )
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
@@ -28,7 +33,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
