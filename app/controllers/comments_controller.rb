59825d7b7e47294dc293592291d3f545c050f885
rajesh1285 <rajeshnuthalapati499@gmail.com>
2018-04-22 07:46:02 +0530

    changes done

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,8 +2,8 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!
 
   def create 
-      user_role = Todo.find_by(id: params[:todo_id]).user.role if  params[:todo_id].present?
-      if user_role == current_user.role || current_user.expert?
+      @user_role = Todo.find_by(id: params[:todo_id]).user.role if  params[:todo_id].present?
+      if (@user_role == current_user.role) || current_user.expert?
         @comment = @commentable.comments.new comment_params
         @comment.user = current_user
         if @comment.save
@@ -17,7 +17,6 @@ class CommentsController < ApplicationController
   end
 
   private
-
     def comment_params
       params.require(:comment).permit(:title)
     end
